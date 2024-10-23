5723e8a9b73159b380c22fc0d8f89f264ee79d44
Adeolu Amole <adeoluamole@gmail.com>
2024-10-23 14:22:58 +0100

    updates

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,7 @@ import passport from "passport";
 const router = Router();
 
 const client_URL = "https://titan-super.vercel.app";
+// const client_URL = "http://localhost:5173";
 
 
 router.get(
@@ -14,13 +15,17 @@ router.get(
     scope: ["profile", "email"],
   })
 );
-router.get(
-  "/google/callback",
+
+
+router.get("/callback/google", (req, res, next) => {
+  console.log("Google callback hit"); // Log when the callback is accessed
   passport.authenticate("google", {
-    successRedirect: client_URL,
-    failureRedirect: "/login/failed",
-  })
-);
+      successRedirect: client_URL,
+      failureRedirect: "/login/failed",
+  })(req, res, next);
+});
+
+
 
 router.get("/login/failed", (req, res) => {
   res.status(401).json({
